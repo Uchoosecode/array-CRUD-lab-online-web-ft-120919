0ad0b7ea35b65e574cbe92e9f0452dbbb439b143
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-10 21:43:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  return array[index_number] = element
+  array[index_number] = element
+  return element
 end
 
 def retrieve_first_element_from_array(array)
